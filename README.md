eb041a8fe058be69c6de8b4f6254d7b30a79b53a
Karan Shukla <shuklak13@gmail.com>
2018-03-29 16:28:46 -0500

    add README and command-line args

---
+++ README.md
@@ -0,0 +1,9 @@
+# Data
+
+We received our data from the [University of California San Diego's Amazon dataset](http://jmcauley.ucsd.edu/data/amazon/). In particular, the dataset we used was "reviews_Amazon_Instant_Video_5.json.gz", a set of reviews for Amazon Instant Video content, but our technique should work on any of the 5-core datasets they provided.
+
+
+# Code Execution
+
+1. Run "python parse.py (data_file) (pickle_file)" to parse the data file into a Python dictionary, which is stored in a pickle file. The default names are "reviews_Amazon_Instant_Video_5.json.gz" and "products.pkl". This step only needs to be run once per dataset.
+2. Run "python sample.py (pickle_file)" to ...???. The default pickle file is "products.pkl".
--- parse.py
+++ parse.py
@@ -1,4 +1,7 @@
-data_path = "reviews_Amazon_Instant_Video_5.json.gz"
+import sys
+data_path = sys.argv[1] if len(sys.argv)>1  else "reviews_Amazon_Instant_Video_5.json.gz"
+pkl_path  = sys.argv[2] if len(sys.argv)>2  else "products.pkl"
+
 def parse(path):
     from ReviewTuple import ReviewTuple
     import gzip
@@ -11,13 +14,14 @@ products = defaultdict(list)
 users = defaultdict(list)
 
 import animation
-print("Parsing review data... may take a minute...")
+print("Parsing review data from " + data_path + "... \nThis might take a minute...")
 animation.start()
 for review in parse(data_path):
     products[review.product].append(review)
     users[review.user].append(review)
 
 import pickle
-with open("products.pkl", 'wb') as f:
+with open(pkl_path, 'wb') as f:
     pickle.dump(products, f)
 animation.end()
+print("Parsed review data stored in " + pkl_path)
