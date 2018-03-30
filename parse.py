da0c6ddec6249c0635d184b734ae07f999404716
Karan Shukla <shuklak13@gmail.com>
2018-03-30 16:45:21 -0500

    refactor parse; now outputs user.pkl + products.pkl

--- parse.py
+++ parse.py
@@ -1,30 +1,36 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--data', help="data file containing Amazon review data (input of parse.py", default="reviews_Amazon_Instant_Video_5.json.gz")
-parser.add_argument('-p', '--pickle', help="pickle file containing parsed review data (output of parse.py)", default="products.pkl")
-args = parser.parse_args()
-print("Command-line args:" + str(args.__dict__))
-
-def parse(path):
-    from ReviewTuple import ReviewTuple
-    import gzip
-    g = gzip.open(path, 'r')
-    for l in g:
-        yield ReviewTuple(eval(l))
-
+# Import and Initialize
+import animation, pickle, argparse
 from collections import defaultdict
 products = defaultdict(list)
 users = defaultdict(list)
 
-import animation
+# Read-in Command-Line Arguments
+cmdlnparser = argparse.ArgumentParser()
+cmdlnparser.add_argument('-d', '--data', help="data file containing Amazon review data (input", default="reviews_Amazon_Instant_Video_5.json.gz")
+cmdlnparser.add_argument('-p', '--productpickle', help="pickle file containing dictionary mapping each product to its reviews (output)", default="products.pkl")
+cmdlnparser.add_argument('-u', '--userpickle', help="pickle file containing dictionary mapping each user to his or her reviews (output)", default="users.pkl")
+args = cmdlnparser.parse_args()
+print("Command-line args:" + str(args.__dict__))
+
+# Function for parsing json.gz files
+def parse(path):
+    import gzip
+    from ReviewTuple import ReviewTuple
+    g = gzip.open(path, 'r')
+    for l in g:
+        yield ReviewTuple(eval(l))
+
+# Parse json.gz file into "products" and "users" dictionaries
 print("Parsing review data from " + args.data + "... \nThis might take a minute...")
 animation.start()
 for review in parse(args.data):
     products[review.product].append(review)
     users[review.user].append(review)
 
-import pickle
-with open(args.pickle, 'wb') as f:
+# Store "products" and "users" in pickle files
+with open(args.productpickle, 'wb') as f:
     pickle.dump(products, f)
+with open(args.userpickle, 'wb') as f:
+    pickle.dump(users, f)
 animation.end()
-print("Parsed review data stored in " + args.pickle)
+print("Parsed review data stored in " + args.productpickle + " and " + args.userpickle)
