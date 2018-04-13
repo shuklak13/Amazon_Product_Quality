182d0d96864bce8c98012cd0bc45d250d2672558
Karan Shukla <shuklak13@gmail.com>
2018-04-13 17:17:42 -0500

    fix type conversion error

--- evaluate.py
+++ evaluate.py
@@ -3,30 +3,7 @@ with open("pkl/product_probabilities.pkl", 'rb') as f:
     probabilities = pickle.load(f)
 with open("pkl/amzn_ratings.pkl", 'rb') as f:
     ratings = pickle.load(f)
-    ratings = {product: ratings[product] for product in ratings if ratings[product] is not None}
-
-# ratings = {
-#     "B006Y5BWQG": 4.7,
-#     "B007943UZ2": 3.8,
-#     "B00EDG3GQ2": 4.5,
-#     "B009IF7092": 4.8,
-#     "B00HBJ9XGU": 4.5,
-#     "B00BG69SEQ": 4.5,
-#     "B00KD9N9BA": 4.3,
-#     "B00AEFVNFC": 3.5,
-#     "B002OLLH46": 3.1,
-#     "B001F6ZIXC": 4.7,
-#     "B00978LMCG": 2.4,
-#     "B003UU1UTC": 4.4,
-#     "B008XFAZRW": 4.5,
-#     "B00IAKMVHM": 3.7,
-#     "B00DMLRI0O": 2.4,
-#     "B003FGKBSC": 4.7,
-#     "B008JSNIQS": 4.6,
-#     "B009M9GI06": 3.1,
-#     "B007WPLZAK": 4.1,
-#     "B00EY7YGBO": 4.7
-# }
+    ratings = {product: ratings[product] for product in ratings if ratings[product] is not ''}
 
 from numpy import array, corrcoef
 from scipy.stats import linregress
@@ -41,7 +18,7 @@ print(linregress(x,y))
 
 
 # Correlations
-print (corrcoef(x,y)[0,1])
+print ("Correlation", corrcoef(x,y)[0,1])
 
 # Scatter Plots 
 plt.scatter(x,y)
